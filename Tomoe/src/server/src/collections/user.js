fc4689d432e8d7732b1e8fb9729116aee8e9f07b
Shubham Naik <shubham.naik10@gmail.com>
2017-07-02 00:54:07 -0700

    feature(Tomoe) Hacker Class + Tests

--- Tomoe/src/server/src/collections/user.js
+++ Tomoe/src/server/src/collections/user.js
@@ -1,6 +1,7 @@
 import { aql } from 'arangojs';
 import uuidv4 from 'uuid/v4'
 import sha256 from 'sha256';
+import Boom from 'boom';
 
 export class User{
   constructor(data = {}){
@@ -56,6 +57,14 @@ export class User{
    this.temp_password = null; // sets any temp password to none
  }
 
+ _setUserData(user){
+   for(let i in this){
+     if((this[i] && typeof this[i] !== '[object Function]') && user[i]){
+       this[i] = user[i];
+     }
+   }
+ }
+
   setEmail(email){
     this.email = email;
   }
@@ -72,47 +81,84 @@ export class User{
     return confirmedPassword === this.temp_password;
   }
 
+  static query(params){
+    return new Promise((resolve, reject) => {
 
+      const collection = this.getCollection();
+      let query = `FOR user IN @@collection `;
+      let binds = { '@collection': collection.name }
+      if(params){
+        params.forEach((param) => {
+          const searchBy = param.root + ((param.option) ? ('.' + param.option) : '');
+          const searchByBinder = searchBy.replace(/\./g, '_');
+          const operator = param.operator || '==';
+          query += `FILTER user.${searchBy} ${operator} @${searchByBinder} `;
+          binds[searchByBinder] = param.data;
+        })
+      }
 
-  static find(userData){
+      query += 'RETURN user';
+
+      db.query(query, binds ).then((cursor) => {
+        if(!cursor){
+          reject(Boom.badImplementation('Something went wrong...'))
+          return;
+        }
+
+       cursor.all().then((users) => {
+         resolve(users);
+        }).catch((err) => {
+          reject(Boom.badImplementation(err));
+        });
+      }).catch((err) => {
+        reject(Boom.badImplementation(err));
+      });
+    });
+  }
+
+  static find(userData, doNotFind = false){
     return new Promise((resolve, reject) => {
       const collection = this.getCollection();
 
       if(!userData || (userData && !userData.email && !userData.id)){
-        reject('You did not provide an id or email');
+        reject(Boom.badRequest('You did not provide an id or email'));
         return;
       }
 
-      let query;
 
-      if(userData.id){
-         query = aql`FOR u IN ${collection}
-                        FILTER u.id == ${userData.id}
-                        RETURN u`;
-      } else {
-        query = aql`FOR u IN ${collection}
-                          FILTER u.email == ${userData.email}
-                          RETURN u`;
-      }
+      const searchBy = (userData.id) ? 'id' : 'email';
+      let query = `FOR user IN @@collection ` +
+                  `FILTER user.${searchBy} == @searchBy ` +
+                  'RETURN user';
 
-      db.query(query, (err, cursor) => {
-         if(err){
-           reject(err);
-           return;
-         }
 
-         cursor.all(function(err, users){
-            if(err){
-              reject(err);
+      db.query(query, { '@collection': collection.name, searchBy: userData[searchBy] } ).then((cursor) => {
+        if(!cursor){
+          reject(Boom.badImplementation('Something went wrong...'))
+          return;
+        }
+         cursor.all().then((users) => {
+            if(users && users[0] && users[0].email){
+              if(doNotFind) {
+                reject(Boom.badRequest(`A user exists with this ${searchBy}!`));
+                return;
+              }
+
+              resolve(users[0]);
               return;
             }
 
-            if(users && users[0] && users[0].email){
-              resolve(users[0]); // we found a user!
-            } else {
-              resolve(false);
+            if(doNotFind) {
+              resolve();
+              return;
             }
+
+            reject(Boom.notFound(`No user found with that ${searchBy} exists`));
+          }).catch((err) => {
+            reject(Boom.badImplementation(err));
           });
+      }).catch((err) => {
+        reject(Boom.badImplementation(err));
       });
     });
   }
@@ -123,17 +169,65 @@ export class User{
 
         if(this._validatePasswords(this.temp_password, user.password)){
 
-          for(let i in this){
-            if((this[i] && typeof this[i] !== '[object Function]') && user[i]){
-              this[i] = user[i];
-            }
-          }
 
+          this._setUserData(user);
           resolve(user);
           return;
         }
 
-        reject('Passwords do not match');
+        reject(Boom.badRequest('Passwords do not match'));
+      }).catch((err) => {
+        reject(err);
+      });
+    });
+  }
+
+
+  _modifyUserObject(newData, original = this, root = true){
+		for(let i in newData){
+  		if(newData[i] && (newData[i].constructor === Object || newData[i].constructor === Array)){
+      	original[i] = this._modifyUserObject(newData[i], original[i], false)
+      } else if(newData[i] === '$delete'){
+      	delete original[i];
+			} else {
+				original[i] = newData[i];
+			}
+    }
+
+    if(!root){
+      return original;
+    }
+  }
+
+  update(newData){
+    return new Promise((resolve, reject) => {
+      const collection = this.constructor.getCollection();
+      this.constructor.find({ id: this.id }).then((user) => {
+          this._modifyUserObject(newData);
+          let updatedUser = this._get();
+          delete updatedUser.id;
+
+          collection.update(user, updatedUser).then((user) => {
+            if(user){
+              this.constructor.find({ email: this.email }).then((user) => {
+                 this._setUserData(user);
+                 resolve(user);
+                 return;
+              }).catch((err) => {
+                reject(err);
+                return;
+              });
+
+              return;
+            }
+
+            reject(Boom.badImplementation('There was an issue creating your user!'));
+          }).catch((err) => {
+            reject(Boom.badImplementation(err));
+          });
+
+          return;
+
       }).catch((err) => {
         reject(err);
       });
@@ -142,34 +236,20 @@ export class User{
 
   save(){
     return new Promise((resolve, reject) => {
-      this.constructor.find({ email: this.email }).then((user) => {
-        if(user){
-           reject('This user exists!');
-           return;
-        }
-
+      this.constructor.find({ email: this.email }, true).then(() => {
         this._securePassword();
         let userDetails = this._get();
-        this.constructor.getCollection().save(userDetails, (err, meta) =>{
-          if(err){
-            reject(err);
-            return;
-          }
-
+        this.constructor.getCollection().save(userDetails).then((user) =>{
           this.constructor.find({ email: this.email }).then((user) => {
-            if(user){
+               this._setUserData(user);
                resolve(user);
                return;
-            }
-
-            reject('There was an issue creating your user!');
-            return;
-
           }).catch((err) => {
-
-            reject(err);
+            reject(Boom.badImplementation(err));
             return;
           });
+        }).catch((err) => {
+          reject(Boom.badImplementation(err));
         });
       }).catch((err) => {
         reject(err);
@@ -181,7 +261,7 @@ export class User{
 
 export class Admin extends User{
   constructor(data = {}){
-    super(data, 'admin');
+    super(data);
     this.permissions = data.permissions;
 
   }
@@ -199,4 +279,29 @@ export class Admin extends User{
       permissions: this.permissions
     }
   }
+}
+
+export class Hacker extends User{
+  constructor(data = {}){
+    super(data);
+
+    this.details = data.details || {};
+    this.status = data.status;
+  }
+
+  static getCollection(){
+    return db.collection('hacker');
+  }
+
+  _get(){
+    return {
+      id: this.id,
+      name: this.name,
+      email:this.email,
+      password:this.password,
+      status: this.status,
+      details: this.details
+    }
+  }
+
 }
--- Tomoe/src/server/tests/collections/user.spec.js
+++ Tomoe/src/server/tests/collections/user.spec.js
@@ -1,46 +1,61 @@
-import assert from 'assert';
-import { User } from '../../src/collections/user';
+import { assert } from 'chai';
+import { User, Admin, Hacker } from '../../src/collections/user';
 
 const sampleUser = {
   name: 'Shubham Naik',
   email: 'shub@hackmerced.com',
-  temp_password: 'testing2'
+  temp_password: 'testing2',
+  newName: 'Shubham D Naik',
+  permissions: 'rw',
+  details: {
+    age: 20
+  }
 }
 
-describe('User', function() {
+const sampleUser2 = {
+  name: 'Jan Tanja',
+  email: 'jan@hackmerced.com',
+  temp_password: 'testing3',
+  permissions: 'rw',
+  details: {
+    age: 20
+  }
+}
+
+describe('User', () =>  {
   let testUser;
 
-  describe('#constructor', function() {
-    it('accesss the User class', function() {
+  describe('#constructor', () =>  {
+    it('accesss the User class', () =>  {
       testUser = new User({ name: sampleUser.name} );
       assert.equal(testUser.name, sampleUser.name);
     });
   });
 
-  describe('#setEmail', function() {
-    it('sets a user email', function() {
+  describe('#setEmail', () =>  {
+    it('sets a user email', () =>  {
       testUser.setEmail(sampleUser.email);
       assert.equal(testUser.email, sampleUser.email);
     });
   });
 
-  describe('#setPassword', function() {
-    it('sets a temporary password', function() {
+  describe('#setPassword', () =>  {
+    it('sets a temporary password', () =>  {
       testUser.setPassword(sampleUser.temp_password);
       assert.equal(testUser.temp_password, sampleUser.temp_password);
     });
   });
 
-  describe('#confirmPassword', function() {
-    it('can confirm a password ', function() {
+  describe('#confirmPassword', () =>  {
+    it('can confirm a password ', () =>  {
       const confirmStatus = testUser.confirmPassword(sampleUser.temp_password);
       assert.equal(confirmStatus, true);
     });
   });
 
-  describe('#save', function() {
+  describe('#save', () =>  {
 
-    it('can save a user to a database ', function(done) {
+    it('can save a user to a database ', (done) =>  {
       testUser.save().then((user) => {
         assert.equal(testUser.name, sampleUser.name);
         done();
@@ -49,20 +64,20 @@ describe('User', function() {
       });
     });
 
-    it('can prevent overwriting an existing user ', function(done) {
+    it('can prevent overwriting an existing user ', (done) =>  {
       testUser.save().then((user) => {
         done('A user was created somehow!');
       }).catch((err) => {
-        assert.equal('This user exists!', err);
+        assert.equal('A user exists with this email!', err.message);
         done();
       });
     });
   });
 
-  describe('#find', function() {
+  describe('#find', () =>  {
     let catchUser;
 
-    it('can find an existing user with email', function(done) {
+    it('can find an existing user with email', (done) =>  {
       User.find({email: sampleUser.email}).then((user) => {
         catchUser = user;
         assert.equal(catchUser.name, sampleUser.name);
@@ -72,7 +87,7 @@ describe('User', function() {
       });
     });
 
-    it('can find an existing user with id ', function(done) {
+    it('can find an existing user with id ', (done) =>  {
       User.find({id: catchUser.id}).then((user) => {
         assert.equal(testUser.name, catchUser.name);
         done();
@@ -81,39 +96,87 @@ describe('User', function() {
       });
     });
 
-    it('can report an error if no user email or id is provided', function(done) {
+    it('can report an error if no user email or id is provided', (done) =>  {
       User.find().then((user) => {
         done('This should not happen');
       }).catch((err) => {
-        assert.equal('You did not provide an id or email', err);
+        assert.equal('You did not provide an id or email', err.message);
         done();
       });
     });
 
-    it('can tell you it cannot find a user', function(done) {
+    it('can tell you it cannot find a user', (done) =>  {
       User.find({email: 'blah'}).then((user) => {
-        assert.equal(user, false);
-        done();
+        done('This should not happen');
       }).catch((err) => {
-        done(err);
+        assert.equal('No user found with that email exists', err.message);
+        done();
       });
     });
   });
 
-  describe('#validate', function() {
-    it('can notify if your passwords do not match', function(done) {
-      testUser.setPassword('blah');
-      testUser.validate().then(() => {
+  describe('#_modifyUserObject', () =>  {
+    const originalData = {
+			data: "b",
+      data2: {
+      	next: {
+					internal: true,
+        	souwu: true
+        },
+        next2: 'test2',
+        next3: ['one', 'two']
+			},
+      data3: 'test3',
+    }
+
+    const modify = {
+			data: "b",
+      data2: {
+      	next: {
+					internal: false,
+        },
+        next2: 'test2',
+        next3: ['one', 'two', 'three']
+			},
+      data3: '$delete',
+    }
+
+    const modifiedData = {
+			data: "b",
+      data2: {
+      	next: {
+					internal: false,
+        	souwu: true,
+        },
+        next2: 'test2',
+        next3: ['one', 'two', 'three']
+			}
+    }
+
+    it('update a user object', () =>  {
+      const finalData = testUser._modifyUserObject(modify, originalData, false);
+      assert.equal(JSON.stringify(modifiedData), JSON.stringify(finalData));
+    });
+  });
+
+  describe('#update', () =>  {
+    let tempId;
+    it('can not update a user if that user does not exists', (done) =>  {
+      tempId = testUser.id;
+      testUser.id = 'blah';
+      testUser.update().then(() => {
         done('This should not happen');
       }).catch((err) => {
-        assert.equal('Passwords do not match', err);
+        assert.equal('No user found with that id exists', err.message);
         done();
       });
     });
 
-    it('can validate your user account (login operation)', function(done) {
-      testUser.setPassword(sampleUser.temp_password);
-      testUser.validate().then((user) => {
+    it('can update your user account', (done) =>  {
+      testUser.id = tempId;
+      sampleUser.name = sampleUser.newName;
+      testUser.setName(sampleUser.name);
+      testUser.update().then((user) => {
         assert.equal(user.name, sampleUser.name);
         done();
       }).catch((err) => {
@@ -122,5 +185,184 @@ describe('User', function() {
     });
   });
 
+  describe('#query', () =>  {
+    before((done) => {
+      // save another user
+      let testUser2 = new User({ name: sampleUser2.name, email: sampleUser2.email });
+
+      testUser2.setPassword(sampleUser2.temp_password);
+
+      testUser2.save().then(() => {
+         done();
+      }).catch((err) => {
+        throw 'Failed to create second user:' + err;
+      });
+    });
+
+    it('can return a list of users', (done) =>  {
+      User.query().then((users) => {
+        assert.equal(users.length, 2);
+        done();
+      }).catch((err) => {
+        done(err);
+      });
+    });
+
+    it('can return a list of users based on a query', (done) =>  {
+      const params = [
+        {
+          root: 'email',
+          data: sampleUser.email
+        }
+      ]
+
+      User.query(params).then((users) => {
+        assert.equal(users[0].email, params[0].data);
+        done();
+      }).catch((err) => {
+        done(err);
+      });
+    });
+
+    it('can return a list of users based on multiple queries', (done) =>  {
+      const params = [
+        {
+          root: 'email',
+          data: sampleUser.email
+        },
+        {
+          root: 'name',
+          data: sampleUser.name
+        }
+      ]
+
+      User.query(params).then((users) => {
+        assert.equal(users[0].email, params[0].data);
+        done();
+      }).catch((err) => {
+        done(err);
+      });
+    });
+
+    it('can return an empty list of users', (done) =>  {
+      const params = [
+        {
+          root: 'email',
+          data: 'blah'
+        }
+      ]
+
+      User.query(params).then((users) => {
+        assert.equal(users.length, 0);
+        done();
+      }).catch((err) => {
+        done(err);
+      });
+    });
+  });
+
+  describe('#validate', () =>  {
+    it('can notify if your passwords do not match', (done) =>  {
+      testUser.setPassword('blah');
+      testUser.validate().then(() => {
+        done('This should not happen');
+      }).catch((err) => {
+        assert.equal('Passwords do not match', err.message);
+        done();
+      });
+    });
+
+    it('can validate your user account (login operation)', (done) =>  {
+      testUser.setPassword(sampleUser.temp_password);
+      testUser.validate().then((user) => {
+        assert.equal(user.name, sampleUser.newName);
+        done();
+      }).catch((err) => {
+        done(err);
+      });
+    });
+  });
+});
+
+
+describe('Admin', () =>  {
+  let testAdmin;
+
+  describe('#constructor', () =>  {
+    it('accesss the Admin class', () =>  {
+      testAdmin = new Admin({ permissions: sampleUser.permissions} );
+      assert.equal(testAdmin.permissions, sampleUser.permissions);
+    });
+  });
+
+});
+
+describe('Hacker', () =>  {
+  let testHacker;
+
+  describe('#constructor', () =>  {
+    it('accesss the User class', () =>  {
+      const hackerData = {
+        name: sampleUser.name,
+        email: sampleUser.email,
+        details: {
+          age: sampleUser.details.age
+        }
+      }
+
+      testHacker = new Hacker(hackerData);
+      testHacker.setPassword(sampleUser.temp_password);
+
+      assert.include(testHacker, hackerData);
+    });
+  });
+
+  describe('#query', () =>  {
+    before((done) => {
+      testHacker.save().then(() => {
+        done();
+      }).catch((err) => {
+        throw 'Failed to create a hacker:' + err;
+      });
+    });
+
+    it('can query hackers with data inside of objects inside documents', (done) =>  {
+      const params = [
+        {
+          root: 'details',
+          option: 'age',
+          data: sampleUser.details.age
+        }
+      ]
+
+      Hacker.query(params).then((users) => {
+        assert.equal(users[0].details.age, params[0].data);
+        done();
+      }).catch((err) => {
+        done(err);
+      });
+     });
+
+     it('can query hackers with data inside of objects inside documents and normal values', (done) =>  {
+       const params = [
+         {
+           root: 'details',
+           option: 'age',
+           data: sampleUser.details.age
+         },
+         {
+           root: 'name',
+           data: sampleUser.name
+         }
+       ]
+
+       Hacker.query(params).then((users) => {
+         assert.equal(users[0].details.age, params[0].data);
+         done();
+       }).catch((err) => {
+         done(err);
+       });
+      });
+  });
 
 });
