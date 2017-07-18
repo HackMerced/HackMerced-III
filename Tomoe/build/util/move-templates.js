1c74342866eedc854ec053a5311ebc4425adca18
Shubham Naik <shubham.naik10@gmail.com>
2017-07-18 13:46:09 -0700

    feature(Tomoe) Error on DB connection failure

--- Tomoe/build/util/move-templates.js
+++ Tomoe/build/util/move-templates.js
@@ -5,7 +5,11 @@ const SERVER_DIR = './src/server/src/templates/'
 const BUILD_DIR = './build/templates/'
 
 export function moveFile(fileName){
-  if(!fs.existsSync(SERVER_DIR + fileName)){
-    fs.createReadStream(BUILD_DIR + fileName).pipe(fs.createWriteStream(SERVER_DIR + fileName));
-  }
+  return new Promise((resolve, reject) => {
+    if(!fs.existsSync(SERVER_DIR + fileName)){
+      fs.createReadStream(BUILD_DIR + fileName).pipe(fs.createWriteStream(SERVER_DIR + fileName));
+    }
+
+    resolve();
+  })
 }
