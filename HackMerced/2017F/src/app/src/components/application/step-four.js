5cccc0e263edc5cb4a1f7735211d20160b1514e2
Shubham Naik <shubham.naik10@gmail.com>
2017-07-27 10:06:09 -0700

    fix(2017F) Misc UI fixes

--- HackMerced/2017F/src/app/src/components/application/step-four.js
+++ HackMerced/2017F/src/app/src/components/application/step-four.js
@@ -12,32 +12,32 @@ export class StepFour extends Component {
         <div className='apply__code-of-conduct'>
           <p>Our hackathon is dedicated to providing a harassment-free experience for everyone, regardless of the following.</p>
           <ul>
-            <li>gender</li>
-            <li>gender identity and expression</li>
-            <li>age</li>
-            <li>sexual orientation</li>
-            <li>disability</li>
-            <li>physical appearance</li>
-            <li>body size</li>
-            <li>race</li>
-            <li>ethnicity</li>
-            <li>nationality</li>
-            <li>religion</li>
-            <li>previous hackathon attendance or lack of</li>
-            <li>computing experience or lack of</li>
-            <li>chosen programming language or tech stack</li>
+            <li>Gender</li>
+            <li>Gender identity and expression</li>
+            <li>Age</li>
+            <li>Sexual orientation</li>
+            <li>Disability</li>
+            <li>Physical appearance</li>
+            <li>Body size</li>
+            <li>Race</li>
+            <li>Ethnicity</li>
+            <li>Nationality</li>
+            <li>Religion</li>
+            <li>Previous hackathon attendance or lack of</li>
+            <li>Computing experience or lack of</li>
+            <li>Chosen programming language or tech stack</li>
           </ul>
           <p>We do not tolerate harassment of hackathon participants in any form. Sexual language and imagery is not appropriate at any hackathon venue, this includes the following.</p>
           <ul>
-            <li>hacks</li>
-            <li>talks, presentations, or demos</li>
-            <li>workshops</li>
-            <li>any parties associated to the hackathon</li>
-            <li>social media</li>
-            <li>any other online media</li>
+            <li>Hacks</li>
+            <li>Talks, presentations, or demos</li>
+            <li>Workshops</li>
+            <li>Any parties associated to the hackathon</li>
+            <li>Social media</li>
+            <li>Any other online media</li>
           </ul>
           <p>You agree to <a className='link--underlined' target='_blank' href='https://static.mlh.io/docs/mlh-code-of-conduct.pdf'>MLH Code of Conduct</a></p>
-          <p>You agree to the terms of both the <a className='link--underlined' target='_blank' href='https://github.com/MLH/mlh-policies/tree/master/prize-terms-and-conditions'>MLH Contest Terms and Conditions</a> and the <a className='link--underlined' target='_blank' href='https://mlh.io/privacy'>MLH Privacy Policy</a>. Please note that you may receive pre and post-event informational e-mails and occasional messages about hackathons from MLH as per the MLH Privacy Policy.</p>
+          <p>You also agree to the terms of both the <a className='link--underlined' target='_blank' href='https://mlh.io/privacy'>MLH Privacy Policy</a> and the <a className='link--underlined' target='_blank' href='https://github.com/MLH/mlh-policies/tree/master/prize-terms-and-conditions'>MLH Contest Terms and Conditions</a>. Please note that you may receive pre and post-event informational e-mails and occasional messages about hackathons from MLH as per the MLH Privacy Policy.</p>
           <p>Hackathon participants violating these rules may be sanctioned or expelled from the hackathon at the discretion of the hackathon organisers.</p>
         </div>
 
