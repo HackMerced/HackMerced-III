ceed8ab5a764ca30626a8fa8be9ede97e75dcb4a
Artsyy <jxie9@ucmerced.edu>
2017-09-04 17:56:42 -0700

    added email field

--- HackMerced/2017F/src/server/src/validators/volunteer.js
+++ HackMerced/2017F/src/server/src/validators/volunteer.js
@@ -19,7 +19,7 @@ export const volunteerValidators = {
 		payload:{
 			name: Joi.string().required(),
 			age: Joi.number().integer().min(14).max(121).required().options({ language: { any: { allowOnly: 'Sorry we only allow people older than age 14 and younger than 121!' } } }),
-			status: Joi.required(),
+			email: Joi.string().required(),
 			availability: Joi.string().required(),
 			dietary_restrictions: Joi.string().required(),
 			shirt_size: Joi.string().required()
