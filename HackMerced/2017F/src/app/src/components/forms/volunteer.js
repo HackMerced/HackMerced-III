5df55ab17d7eff1bc60656bab8d7384fa8fd1237
Shubham Naik <shubham.naik10@gmail.com>
2017-09-23 21:47:57 -0700

    feature(2017F/volunteers) update import

--- HackMerced/2017F/src/app/src/components/forms/volunteer.js
+++ HackMerced/2017F/src/app/src/components/forms/volunteer.js
@@ -15,37 +15,39 @@ const assign = Object.assign || require('object.assign');
 export class VolunteerForm extends Component {
 
   render() {
+    const { errors, data } = this.props;
+
     return (
       <form onChange={this._onChange.bind(this)} onSubmit={this._onSubmit.bind(this)} >
         <h3>Interested in helping out HackMerced?</h3>
         <h3> Become a volunteer!</h3>
         <TextInputBlock
-          error={this.props.errors.name}
-          value={this.props.data.name}
+          error={errors.name}
+          value={data.name}
           name='name'
           type='text'
           label='Name'
           placeholder='Your Full Name' autoCorrect="off" autoCapitalize="off" spellCheck="false"/>
 
         <TextInputBlock
-          error={this.props.errors.email}
-          value={this.props.data.email}
+          error={ errors.email}
+          value={data.email}
           name='email'
           type='text'
           label='Email'
           placeholder='Your Email' autoCorrect="off" autoCapitalize="off" spellCheck="false"/>
 
         <TextInputBlock
-          error={this.props.errors.age}
-          value={this.props.data.age}
+          error={errors.age}
+          value={data.age}
           name='age'
           type='number'
           label='Age'
           placeholder='Your Age' autoCorrect="off" autoCapitalize="off" spellCheck="false"/>
 
         <TextInputBlock
-          value={this.props.data.dietary_restrictions}
-          error={this.props.errors.dietary_restrictions}
+          value={data.dietary_restrictions}
+          error={errors.dietary_restrictions}
           label='Do you have any dietary restrictions?'
           labelType='large'
           name='dietary_restrictions'
@@ -63,8 +65,8 @@ export class VolunteerForm extends Component {
           ]}/>
 
         <TextInputBlock
-          value={this.props.data.shirt_size}
-          error={this.props.errors.shirt_size}
+          value={data.shirt_size}
+          error={errors.shirt_size}
           label='Shirt Size'
           name='shirt_size'
           type='options'
@@ -82,8 +84,8 @@ export class VolunteerForm extends Component {
           ]}/>
 
         <TextInputBlock
-          value={this.props.data.friday_availability}
-          error={this.props.errors.friday}
+          value={data.friday_availability}
+          error={errors.friday}
           label='Friday Availability'
           labelType='large'
           name='friday_availability'
@@ -98,8 +100,8 @@ export class VolunteerForm extends Component {
           ]}/>
 
         <TextInputBlock
-          value={this.props.data.saturday_availability}
-          error={this.props.errors.saturday}
+          value={data.saturday_availability}
+          error={errors.saturday}
           label='Saturday Availability'
           labelType='large'
           name='saturday_availability'
@@ -114,8 +116,8 @@ export class VolunteerForm extends Component {
           ]}/>
 
         <TextInputBlock
-          value={this.props.data.sunday_availability}
-          error={this.props.errors.sunday}
+          value={data.sunday_availability}
+          error={errors.sunday}
           label='Sunday Availability'
           labelType='large'
           name='sunday_availability'
@@ -145,7 +147,7 @@ export class VolunteerForm extends Component {
 
   // Merges the current state with a change
   _mergeWithCurrentState(change) {
-    return assign(this.props.data, change);
+    return assign(data, change);
   }
 
   // Emits a change of the form state to the application state
@@ -154,18 +156,20 @@ export class VolunteerForm extends Component {
   }
 
   _onSubmit(event){
+    const { dispatch, data } = this.props;
+
     event.preventDefault();
 
-    this.props.dispatch(signUpVolunteer({
-      name: this.props.data.name,
-      email: this.props.data.email,
-      age: this.props.data.age,
+    dispatch(signUpVolunteer({
+      name: data.name,
+      email: data.email,
+      age: data.age,
       availability: {
-        friday: this.props.data.friday_availability,
-        saturday: this.props.data.saturday_availability,
-        sunday: this.props.data.sunday_availability
+        friday: data.friday_availability,
+        saturday: data.saturday_availability,
+        sunday: data.sunday_availability
       },
-      dietary_restrictions: this.props.data.dietary_restrictions,
+      dietary_restrictions: data.dietary_restrictions,
       shirt_size: this.props.data.shirt_size
     }))
 
